1e2b966f3203b91aec7385b4d83eb3cf6e230b31
Nicolás Marlu <nicolasmarluti18pro@outlook.com>
2026-05-15 17:32:03 -0600

    added link

--- src/data/social.ts
+++ src/data/social.ts
@@ -3,11 +3,13 @@ export const socialMediaES = [
     {
       label: 'LinkedIn',
       value: 'linkedin.com/in/nicolasmarlu',
+      link: 'https://linkedin.com/in/nicolasmarlu',
       description: 'Para oportunidades profesionales, colaboración técnica o proyectos.',
     },
     {
       label: 'Github',
       value: 'github.com/nicolasmarlu',
+      link: 'https://github.com/nicolasmarlu',
       description: 'Repositorios, experimentos, arquitectura y proyectos personales.',
     }
 
