728dcfc4fa913c8e37b1fbad7528927b2474d06f
Nicolás Marlu <nicolasmarluti18pro@outlook.com>
2026-05-18 22:27:42 -0600

    added cors settings

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:4200',
-    'https://tudominio.com'
+    'https://xc-portfolio-fsdhuvuh-nicolasmarluprojects.vercel.app'
   ]
 }));
 
